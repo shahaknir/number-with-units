1c44b1dbc0a56a5abfd1ca33d09d69f701d6ecac
shahaknir <62562867+shahaknir@users.noreply.github.com>
2021-06-11 09:17:00 +0300

    ex3 part a submission

--- NumberWithUnits.cpp
+++ NumberWithUnits.cpp
@@ -1,103 +1,341 @@
+//
+// Created by shahak on 11/06/2021.
+//
+
 #include "NumberWithUnits.hpp"
-#include <fstream>
-#include <sstream>
 #include <iostream>
 
 namespace ariel{
 
-    void read_units(ifstream& filetxt){
-        // return filetxt;
+
+    /**
+     * Constructor getting a double const and string
+     * @param x - the amount of the unit
+     * @param str - name of unit
+     */
+    NumberWithUnits::NumberWithUnits(const double &x, const string &str) {
+
     }
-    NumberWithUnits::NumberWithUnits():t(0), temp{""} {}
-    NumberWithUnits::NumberWithUnits(int num, string a){
-        // this->t = num;
-        // this->temp = a;
+
+    /**
+     * get a file and extract the units data into a map
+     * @param ifstream1
+     */
+    void NumberWithUnits::read_units(ifstream &ifstream1) {
+
+    }
+
+    /**
+     * @param numberWithUnits1
+     * @param numberWithUnits2
+     * @return 1 - if the first object is bigger, 0 - if they ar equal, -1 - if the first object is smaller
+     */
+    int compare_units(const NumberWithUnits &numberWithUnits1, const NumberWithUnits &numberWithUnits2) {
+        return 0;
+    }
+
+    /**
+     * input / output methods - get or push an object of this class
+     * @param ostream1 / istream1
+     * @param numberWithUnits
+     * @return
+     */
+    ostream &operator<<(ostream &ostream1, const NumberWithUnits &numberWithUnits) {
+        return ostream1;
+    }
+
+    istream &operator>>(istream &istream1, NumberWithUnits &numberWithUnits1) {
+        return istream1;
+    }
+
+    /**
+     * Overloading the operator (-)
+     * @func1 - (a - b)
+     * @func2 - (a - 5.5)
+     * @func3 - (5.5 - a)
+     * @func4 -     (- a)
+     */
+    NumberWithUnits operator-(const NumberWithUnits &numberWithUnits1, const NumberWithUnits &numberWithUnits2) {
+        return NumberWithUnits();
+    }
+
+    NumberWithUnits operator-(const NumberWithUnits &numberWithUnits, double x) {
+        return NumberWithUnits();
+    }
+
+    NumberWithUnits operator-(double x, const NumberWithUnits &numberWithUnits) {
+        return NumberWithUnits();
+    }
+
+    NumberWithUnits operator-(const NumberWithUnits &numberWithUnits) {
+        return NumberWithUnits();
+    }
+
+    /**
+     * Overloading the operator (+)
+     * @func1 - (a + b)
+     * @func2 - (a + 5.5)
+     * @func3 - (5.5 + a)
+     * @func4 -     (+ a)
+     */
+    NumberWithUnits operator+(const NumberWithUnits &numberWithUnits1, const NumberWithUnits &numberWithUnits2) {
+        return NumberWithUnits();
+    }
+
+    NumberWithUnits operator+(const NumberWithUnits &numberWithUnits1, double x) {
+        return NumberWithUnits();
+    }
+
+    NumberWithUnits operator+(double x, const NumberWithUnits &numberWithUnits) {
+        return NumberWithUnits();
+    }
+
+    NumberWithUnits operator+(const NumberWithUnits &numberWithUnits) {
+        return NumberWithUnits();
+    }
+
+    /**
+     * Overloading the operator (*)
+     * @func1 - (a * b)
+     * @func2 - (a * 5.5)
+     * @func3 - (5.5 * a)
+     */
+    NumberWithUnits operator*(const NumberWithUnits &numberWithUnits1, const NumberWithUnits &numberWithUnits2) {
+        return NumberWithUnits();
+    }
+
+    NumberWithUnits operator*(const NumberWithUnits &numberWithUnits, double x) {
+        return NumberWithUnits();
+    }
+
+    NumberWithUnits operator*(double x, const NumberWithUnits &numberWithUnits) {
+        return NumberWithUnits();
+    }
+
+    /**
+     * Overloading the operator (/)
+     * @func1 - (a / b)
+     * @func2 - (a / 5.5)
+     * @func3 - (5.5 / a)
+     */
+    NumberWithUnits operator/(const NumberWithUnits &numberWithUnits1, const NumberWithUnits &numberWithUnits2) {
+        return NumberWithUnits();
+    }
+
+    NumberWithUnits operator/(const NumberWithUnits &numberWithUnits, double x) {
+        return NumberWithUnits();
+    }
+
+    NumberWithUnits operator/(double, const NumberWithUnits &) {
+        return NumberWithUnits();
+    }
+
+    /**
+     * Overloading the operator (+=)
+     * @func1 - (a += b)
+     * @func2 - (a += 5.5)
+     * @func3 - (5.5 += a)
+     */
+    NumberWithUnits &NumberWithUnits::operator+=(const NumberWithUnits &num) {
+        return *this;
+    }
+
+    NumberWithUnits &NumberWithUnits::operator+=(const double &num) {
+        return *this;
+    }
+
+    NumberWithUnits operator+=(double, NumberWithUnits &num) {
+        return NumberWithUnits();
+    }
+
+    /**
+     * Overloading the operator (-=)
+     * @func1 - (a -= b)
+     * @func2 - (a -= 5.5)
+     * @func3 - (5.5 -= a)
+     */
+    NumberWithUnits operator-=(double, NumberWithUnits &num) {
+        return NumberWithUnits();
+    }
+
+    NumberWithUnits &NumberWithUnits::operator-=(const NumberWithUnits &num) {
+        return *this;
+    }
+
+    NumberWithUnits &NumberWithUnits::operator-=(const double &num) {
+        return *this;
+    }
+
+    /**
+     * Overloading the operator (*=)
+     * @func1 - (a *= b)
+     * @func2 - (a *= 5.5)
+     * @func3 - (5.5 *= a)
+     */
+    NumberWithUnits &NumberWithUnits::operator*=(const NumberWithUnits &num) {
+        return *this;
+    }
+
+    NumberWithUnits &NumberWithUnits::operator*=(const double &num) {
+        return *this;
+    }
+
+    NumberWithUnits operator*=(double x, NumberWithUnits &num) {
+        return NumberWithUnits();
+    }
+
+    /**
+     * Overloading the operator (/=)
+     * @func1 - (a /= b)
+     * @func2 - (a /= 5.5)
+     * @func3 - (5.5 /= a)
+     */
+    NumberWithUnits &NumberWithUnits::operator/=(const NumberWithUnits &num) {
+        return *this;
+    }
+
+    NumberWithUnits &NumberWithUnits::operator/=(const double &num) {
+        return *this;
+    }
+
+    NumberWithUnits operator/=(double x, NumberWithUnits &num) {
+        return NumberWithUnits();
+    }
+
+    /**
+     * Overloading the operator (==)
+     * @func1 - (a == b)
+     * @func2 - (a == 5.5)
+     * @func3 - (5.5 == a)
+     */
+    bool operator==(const NumberWithUnits &num1, const NumberWithUnits &num2) {
+        return false;
+    }
+
+    bool operator==(double x, const NumberWithUnits &num) {
+        return false;
+    }
+
+    bool operator==(const NumberWithUnits &num, double x) {
+        return false;
+    }
+
+    /**
+     * Overloading the operator (!=)
+     * @func1 - (a != b)
+     * @func2 - (a != 5.5)
+     * @func3 - (5.5 != a)
+     */
+    bool operator!=(const NumberWithUnits &num1, const NumberWithUnits &num2) {
+        return false;
+    }
+
+    bool operator!=(const NumberWithUnits &num, double x) {
+        return false;
+    }
+
+    bool operator!=(double x, const NumberWithUnits &num) {
+        return false;
+    }
+
+    /**
+     * Overloading the operator (<)
+     * @func1 - (a < b)
+     * @func2 - (a < 5.5)
+     * @func3 - (5.5 < a)
+     */
+    bool operator<(const NumberWithUnits &num1, const NumberWithUnits &num2) {
+        return false;
+    }
+
+    bool operator<(const NumberWithUnits &num, double x) {
+        return false;
+    }
+
+    bool operator<(double x, const NumberWithUnits &num) {
+        return false;
     }
 
 
-    // Numeric operators
-    NumberWithUnits operator+(NumberWithUnits& a, const NumberWithUnits b){
-        return a;
-    }
-    NumberWithUnits operator+=(NumberWithUnits& a, const NumberWithUnits b){
-        return a;
-    }
-    NumberWithUnits operator-(NumberWithUnits& a, const NumberWithUnits b){
-        return a;
-    }
-    NumberWithUnits operator-=(NumberWithUnits& a, const NumberWithUnits b){
-        return a;
-    }
-    NumberWithUnits operator*(double other, NumberWithUnits& a){
-        return a;
-    }
-    // NumberWithUnits operator*(int other, NumberWithUnits& a){
-        // return a;
-    // }
-                    
-    
-    NumberWithUnits operator+(unsigned int a, NumberWithUnits b){
-        return b;
-    }
-    NumberWithUnits operator+=(unsigned int a, NumberWithUnits b){
-        return b;
-    }
-    NumberWithUnits operator-(unsigned int a, NumberWithUnits b){
-        return b;
-    }
-    NumberWithUnits operator-=(unsigned int a, NumberWithUnits b){
-        return b;
-    }            
-
-    // ++ --
-    NumberWithUnits operator++(NumberWithUnits& a){
-        return a;
-    }
-    NumberWithUnits operator--(NumberWithUnits& a){
-        return a;
+    /**
+     * Overloading the operator (>)
+     * @func1 - (a > b)
+     * @func2 - (a > 5.5)
+     * @func3 - (5.5 > a)
+     */
+    bool operator>(const NumberWithUnits &num1, const NumberWithUnits &num2) {
+        return false;
     }
 
-    // Uneric + & -
-    const NumberWithUnits operator+(NumberWithUnits& a){
-        return a;
+    bool operator>(const NumberWithUnits &num, double x) {
+        return false;
     }
-    const NumberWithUnits operator-(NumberWithUnits& a){
-        return a;
-    }
-            
-    // Comperation Operator Overload
-    bool operator==(const NumberWithUnits& n1, const NumberWithUnits& n2){
-        return true;
-    }
-    bool operator!=(const NumberWithUnits& n1, const NumberWithUnits& n2){
-        return true;
-    }
-    bool operator>(const NumberWithUnits& n1, const NumberWithUnits& n2){
-        return true;
-    }
-    bool operator>=(const NumberWithUnits& n1, const NumberWithUnits& n2){
-        return true;
-    }
-    bool operator<=(const NumberWithUnits& n1, const NumberWithUnits& n2){
-        return true;
-    }
-    bool operator<(const NumberWithUnits& n1, const NumberWithUnits& n2){
-        return true;
-    }
-       
 
-    // Streamming Operator Overload        
-    ostream& operator>>(ostream& input, const NumberWithUnits& num){
-        return input;
+    bool operator>(double x, const NumberWithUnits &num) {
+        return false;
     }
-    ostream& operator<<(ostream& output, const NumberWithUnits& num){
-        return output;
+
+    /**
+     * Overloading the operator (>=)
+     * @func1 - (a >= b)
+     * @func2 - (a >= 5.5)
+     * @func3 - (5.5 >= a)
+     */
+    bool operator>=(const NumberWithUnits &num1, const NumberWithUnits &num2) {
+        return false;
     }
-    istringstream& operator>>(istringstream& input, const NumberWithUnits& num){
-        return input;
+
+    bool operator>=(const NumberWithUnits &num, double x) {
+        return false;
     }
-    istringstream& operator<<(istringstream& output, const NumberWithUnits& num){
-        return output;
+
+    bool operator>=(double x, const NumberWithUnits &num) {
+        return false;
     }
+
+    /**
+     * Overloading the operator (<=)
+     * @func1 - (a <= b)
+     * @func2 - (a <= 5.5)
+     * @func3 - (5.5 <= a)
+     */
+    bool operator<=(const NumberWithUnits &num1, const NumberWithUnits &num2) {
+        return false;
+    }
+
+    bool operator<=(const NumberWithUnits &, double x) {
+        return false;
+    }
+
+    bool operator<=(double x, const NumberWithUnits &num) {
+        return false;
+    }
+
+    /**
+     * Overloading the operator (++)
+     * @func1 - (a++)
+     * @func2 - (++a)
+     */
+    NumberWithUnits &NumberWithUnits::operator++() {
+        return *this;
+    }
+
+    NumberWithUnits NumberWithUnits::operator++(int) {
+        return NumberWithUnits();
+    }
+
+    /**
+     * Overloading the operator (++)
+     * @func1 - (a--)
+     * @func2 - (--a)
+     */
+    NumberWithUnits &NumberWithUnits::operator--() {
+        return *this;
+    }
+
+    NumberWithUnits NumberWithUnits::operator--(int) {
+        return NumberWithUnits();
+    }
+
 
 }
--- NumberWithUnits.hpp
+++ NumberWithUnits.hpp
@@ -1,58 +1,234 @@
-#include <iostream>
+//
+// Created by shahak on 10/06/2021.
+//
+
+#ifndef EX3_PA_NUMBERWITHUNITS_HPP
+#define EX3_PA_NUMBERWITHUNITS_HPP
+
+#pragma once
+#include <map>
 #include <string>
+#include <iostream>
 #include <fstream>
 #include <sstream>
+#include <stdexcept>
+#include <unordered_map>
+
 
 using namespace std;
-
-namespace ariel
-{
+namespace ariel {
     class NumberWithUnits{
-        private:
-            int t;
-            string const temp[5];
+    private:
+        string unit_name;
+        double unit_quantity;
 
-        public:
-            static void read_units(ifstream& filetxt);
 
-            // Constactors
-            NumberWithUnits();
-            NumberWithUnits(int num, string a);
-            
-            // Numeric operators
-            friend NumberWithUnits operator+(NumberWithUnits& a, const NumberWithUnits b);
-            friend NumberWithUnits operator+=(NumberWithUnits& a, const NumberWithUnits b);
-            friend NumberWithUnits operator-(NumberWithUnits& a, const NumberWithUnits b);
-            friend NumberWithUnits operator-=(NumberWithUnits& a, const NumberWithUnits b);
-            friend NumberWithUnits operator*(double other, NumberWithUnits& a);
-            // friend NumberWithUnits operator*(int other, NumberWithUnits& a);
+    public:
+        /**
+         * empty constructor
+        */
+        NumberWithUnits(){
+            this->unit_name = "km";
+            this->unit_quantity = 1;
+        }
 
-            friend NumberWithUnits operator+(unsigned int a, NumberWithUnits b);
-            friend NumberWithUnits operator+=(unsigned int a, NumberWithUnits b);
-            friend NumberWithUnits operator-(unsigned int a, NumberWithUnits b);
-            friend NumberWithUnits operator-=(unsigned int a, NumberWithUnits b);
+        /**
+         * Constructor getting a double const and string
+         * @param x - the amount of the unit
+         * @param str - name of unit
+         */
+        NumberWithUnits(const double& x,const string& str);
 
-            // ++ --
-            friend NumberWithUnits operator++(NumberWithUnits& a);
-            friend NumberWithUnits operator--(NumberWithUnits& a);
+        /**
+         * Destructor - Use to clean unused memory, when the object about to die
+         */
+        ~NumberWithUnits()= default;
 
-            // Uneric + & -
-            friend const NumberWithUnits operator+(NumberWithUnits& a);
-            friend const NumberWithUnits operator-(NumberWithUnits& a);
-            
-            // Comperation Operator Overload
-            friend bool operator==(const NumberWithUnits& n1, const NumberWithUnits& n2);
-            friend bool operator!=(const NumberWithUnits& n1, const NumberWithUnits& n2);
-            friend bool operator>(const NumberWithUnits& n1, const NumberWithUnits& n2);
-            friend bool operator>=(const NumberWithUnits& n1, const NumberWithUnits& n2);
-            friend bool operator<=(const NumberWithUnits& n1, const NumberWithUnits& n2);
-            friend bool operator<(const NumberWithUnits& n1, const NumberWithUnits& n2);
-            
-            // Streamming Operator Overload
-            friend ostream& operator>>(ostream& input, const NumberWithUnits& num);
-            friend ostream& operator<<(ostream& output, const NumberWithUnits& num);
-            friend istringstream& operator>>(istringstream& input, const NumberWithUnits& num);
-            friend istringstream& operator<<(istringstream& output, const NumberWithUnits& num);
+        /**
+         * get a file and extract the units data into a map
+         * @param ifstream1
+         */
+        static void read_units(ifstream& ifstream1);
+
+        /**
+         * @param numberWithUnits1
+         * @param numberWithUnits2
+         * @return 1 - if the first object is bigger, 0 - if they ar equal, -1 - if the first object is smaller
+         */
+        friend int compare_units(const NumberWithUnits& numberWithUnits1, const NumberWithUnits&numberWithUnits2);
+
+        /**
+         * input / output methods - get or push an object of this class
+         * @param ostream1 / istream1
+         * @param numberWithUnits
+         * @return
+         */
+        friend ostream& operator<<(ostream& ostream1, const NumberWithUnits& numberWithUnits);
+        friend istream& operator>>(istream& istream1, NumberWithUnits& numberWithUnits);
+
+        /**
+         * Overloading the operator (-)
+         * @func1 - (a - b)
+         * @func2 - (a - 5.5)
+         * @func3 - (5.5 - a)
+         * @func4 -     (- a)
+         */
+        friend NumberWithUnits operator-(const NumberWithUnits& numberWithUnits1 ,const NumberWithUnits& numberWithUnits2);
+        friend NumberWithUnits operator-(const NumberWithUnits& numberWithUnits, double x);
+        friend NumberWithUnits operator-(double x ,const NumberWithUnits& numberWithUnits);
+        friend NumberWithUnits operator-(const NumberWithUnits& numberWithUnits);
+
+        /**
+         * Overloading the operator (+)
+         * @func1 - (a + b)
+         * @func2 - (a + 5.5)
+         * @func3 - (5.5 + a)
+         * @func4 -     (+ a)
+         */
+        friend NumberWithUnits operator+(const NumberWithUnits& numberWithUnits1,const NumberWithUnits& numberWithUnits2);
+        friend NumberWithUnits operator+(const NumberWithUnits& numberWithUnits1, double x);
+        friend NumberWithUnits operator+(double x,const NumberWithUnits& numberWithUnits);
+        friend NumberWithUnits operator+(const NumberWithUnits& numberWithUnits);
+
+        /**
+         * Overloading the operator (*)
+         * @func1 - (a * b)
+         * @func2 - (a * 5.5)
+         * @func3 - (5.5 * a)
+         */
+        friend NumberWithUnits operator*(const NumberWithUnits& numberWithUnits1,const NumberWithUnits& numberWithUnits2);
+        friend NumberWithUnits operator*(const NumberWithUnits& numberWithUnits, double x);
+        friend NumberWithUnits operator*(double x,const NumberWithUnits& numberWithUnits);
+
+        /**
+        * Overloading the operator (/)
+        * @func1 - (a / b)
+        * @func2 - (a / 5.5)
+        * @func3 - (5.5 / a)
+        */
+        friend NumberWithUnits operator/(const NumberWithUnits& numberWithUnits1,const NumberWithUnits& numberWithUnits2);
+        friend NumberWithUnits operator/(const NumberWithUnits& numberWithUnits, double x);
+        friend NumberWithUnits operator/( double ,const NumberWithUnits&);
+
+        /**
+         * Overloading the operator (+=)
+         * @func1 - (a += b)
+         * @func2 - (a += 5.5)
+         * @func3 - (5.5 += a)
+         */
+        NumberWithUnits &operator+=(const NumberWithUnits &num);
+        NumberWithUnits &operator+=(const double &num);
+        friend NumberWithUnits operator+=( double , NumberWithUnits&num);
+
+        /**
+         * Overloading the operator (-=)
+         * @func1 - (a -= b)
+         * @func2 - (a -= 5.5)
+         * @func3 - (5.5 -= a)
+         */
+        friend NumberWithUnits operator-=(double , NumberWithUnits&num);
+        NumberWithUnits &operator-=(const NumberWithUnits &num);
+        NumberWithUnits &operator-=(const double &num);
+
+        /**
+        * Overloading the operator (*=)
+        * @func1 - (a *= b)
+        * @func2 - (a *= 5.5)
+        * @func3 - (5.5 *= a)
+        */
+        NumberWithUnits &operator*=(const NumberWithUnits &num);
+        NumberWithUnits &operator*=(const double &num);
+        friend NumberWithUnits operator*=(double x, NumberWithUnits&num);
+
+        /**
+        * Overloading the operator (/=)
+        * @func1 - (a /= b)
+        * @func2 - (a /= 5.5)
+        * @func3 - (5.5 /= a)
+        */
+        NumberWithUnits &operator/=(const NumberWithUnits &num);
+        NumberWithUnits &operator/=(const double &num);
+        friend NumberWithUnits operator/=(double x, NumberWithUnits&num);
+
+        /**
+        * Overloading the operator (==)
+        * @func1 - (a == b)
+        * @func2 - (a == 5.5)
+        * @func3 - (5.5 == a)
+        */
+        friend bool operator==(const NumberWithUnits& num1,const NumberWithUnits& num2);
+        friend bool operator==(double x,const NumberWithUnits& num);
+        friend bool operator==(const NumberWithUnits& num, double x);
+
+        /**
+         * Overloading the operator (!=)
+         * @func1 - (a != b)
+         * @func2 - (a != 5.5)
+         * @func3 - (5.5 != a)
+         */
+        friend bool operator!=(const NumberWithUnits& num1,const NumberWithUnits&num2);
+        friend bool operator!=(const NumberWithUnits& num, double x);
+        friend bool operator!=( double x,const NumberWithUnits&num);
+
+        /**
+         * Overloading the operator (<)
+         * @func1 - (a < b)
+         * @func2 - (a < 5.5)
+         * @func3 - (5.5 < a)
+         */
+        friend bool operator<(const NumberWithUnits& num1, const NumberWithUnits& num2);
+        friend bool operator<(const NumberWithUnits& num, double x);
+        friend bool operator<(double x,const NumberWithUnits& num);
+
+        /**
+         * Overloading the operator (>)
+         * @func1 - (a > b)
+         * @func2 - (a > 5.5)
+         * @func3 - (5.5 > a)
+         */
+        friend bool operator>(const NumberWithUnits& num1,const NumberWithUnits& num2);
+        friend bool operator>(const NumberWithUnits& num, double x);
+        friend bool operator>(double x,const NumberWithUnits& num);
+
+        /**
+         * Overloading the operator (>=)
+         * @func1 - (a >= b)
+         * @func2 - (a >= 5.5)
+         * @func3 - (5.5 >= a)
+         */
+        friend bool operator>=(const NumberWithUnits& num1, const NumberWithUnits& num2);
+        friend bool operator>=(const NumberWithUnits& num, double x);
+        friend bool operator>=(double x,const NumberWithUnits& num);
+
+        /**
+         * Overloading the operator (<=)
+         * @func1 - (a <= b)
+         * @func2 - (a <= 5.5)
+         * @func3 - (5.5 <= a)
+         */
+        friend bool operator<=(const NumberWithUnits& num1,const NumberWithUnits& num2);
+        friend bool operator<=(const NumberWithUnits& , double x);
+        friend bool operator<=(double x,const NumberWithUnits& num);
+
+        /**
+         * Overloading the operator (++)
+         * @func1 - (a++)
+         * @func2 - (++a)
+         */
+        NumberWithUnits& operator++();
+        NumberWithUnits operator++(int);
+
+         /**
+          * Overloading the operator (++)
+          * @func1 - (a--)
+          * @func2 - (--a)
+          */
+        NumberWithUnits& operator--();
+        NumberWithUnits operator--(int);
+
 
     };
-} // namespace ariel
+}
+
+
+#endif //EX3_PA_NUMBERWITHUNITS_HPP
